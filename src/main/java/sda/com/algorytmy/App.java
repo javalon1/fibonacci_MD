c989543b56a159995b2251c938f3afb930f2f3b6
Maciej Drobot <maciejdrobot@gmail.com>
2019-05-12 10:55:39 +0100

    Fibonacci FOR bez Tablicy

--- src/main/java/sda/com/algorytmy/App.java
+++ src/main/java/sda/com/algorytmy/App.java
@@ -3,7 +3,8 @@ package sda.com.algorytmy;
 public class App {
     public static void main(String[] args) {
 
-        System.out.println(fibo(8));
+        System.out.println(fibo(10));
+        System.out.println(fiboFor(10));
 
         //Fibonacci FOR i TABLICA
         int[] tab = new int[20];
@@ -15,18 +16,38 @@ public class App {
         }
     }
 
-
     //Fibonacci rekurencja
     public static int fibo(int n) {
-        if (n == 0) {
-            return 0;
-        } else if (n == 1) {
+        if (n <= 2) {
             return 1;
+        } else {
+            return fibo(n - 2) + fibo(n - 1);
         }
-        return fibo(n - 2) + fibo(n - 1);
     }
 
+    //Fibonacci FOR bez tablicy
 
+    public static int fiboFor(int n) {
+        if (n < 2) {
+            return n;
+        } else {
+            int a = 0;
+            int b = 1;
+            int c = 1;
+            int k = 2;
 
-
+            for (k = 2; k < n; k++) {
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            return c;
+        }
+    }
 }
+
+
+
+
+
+
